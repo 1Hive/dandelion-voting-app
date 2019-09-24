cec9fe9f3d3ade3e778af859cbffc2a85c722baf
William Griffiths <willjgriff@gmail.com>
2019-09-24 15:40:24 +0100

    Rename dissent voting to dandelion voting. Update tests to use finished dandelion voting app. Use SafeMath.

--- dissent-oracle/contracts/DissentOracle.sol
+++ dissent-oracle/contracts/DissentOracle.sol
@@ -2,59 +2,70 @@ pragma solidity ^0.4.24;
 
 import "@aragon/os/contracts/apps/AragonApp.sol";
 import "@aragon/os/contracts/acl/IACLOracle.sol";
-import "../../contracts/DissentVoting.sol";
+import "@aragon/os/contracts/lib/math/SafeMath64.sol";
+import "../../contracts/DandelionVoting.sol";
 
-// TODO: SafeMath
-// TODO: Update "DissentVoting" to "DandelionVoting"
 contract DissentOracle is AragonApp, IACLOracle {
 
-    bytes32 public constant SET_DISSENT_VOTING_ROLE = keccak256("SET_DISSENT_VOTING_ROLE");
+    using SafeMath64 for uint64;
+
+    bytes32 public constant SET_DANDELION_VOTING_ROLE = keccak256("SET_DANDELION_VOTING_ROLE");
     bytes32 public constant SET_DISSENT_WINDOW_ROLE = keccak256("SET_DISSENT_WINDOW_ROLE");
 
     string private constant ERROR_NOT_CONTRACT = "DISSENT_ORACLE_NOT_CONTRACT";
 
-    DissentVoting public dissentVoting;
-    uint256 public dissentWindow;
+    DandelionVoting public dandelionVoting;
+    uint64 public dissentWindowBlocks;
 
-    event DissentVotingSet(address dissentVoting);
-    event DissentWindowSet(uint256 dissentWindow);
+    event DandelionVotingSet(address dandelionVoting);
+    event DissentWindowBlocksSet(uint64 dissentWindowBlocks);
 
     /**
     * @notice Initialize the DissentOracle
-    * @param _dissentVoting Dissent voting aragon app address
-    * @param _dissentWindow Time from previous yea vote the oracle will approve a function call for
+    * @param _dandelionVoting Dissent voting aragon app address
+    * @param _dissentWindowBlocks Blocks from previous yea vote the oracle will approve a function call for
     */
-    function initialize(address _dissentVoting, uint256 _dissentWindow) external onlyInit {
-        require(isContract(_dissentVoting), ERROR_NOT_CONTRACT);
+    function initialize(address _dandelionVoting, uint64 _dissentWindowBlocks) external onlyInit {
+        require(isContract(_dandelionVoting), ERROR_NOT_CONTRACT);
 
-        dissentVoting = DissentVoting(_dissentVoting);
-        dissentWindow = _dissentWindow;
+        dandelionVoting = DandelionVoting(_dandelionVoting);
+        dissentWindowBlocks = _dissentWindowBlocks;
 
         initialized();
     }
 
     /**
-    * @notice Update the dissent voting app to `_dissentVoting`
-    * @param _dissentVoting New dissent window
+    * @notice Update the dissent voting app to `_dandelionVoting`
+    * @param _dandelionVoting New dissent window
     */
-    function setDissentVoting(address _dissentVoting) external auth(SET_DISSENT_VOTING_ROLE) {
-        require(isContract(_dissentVoting), ERROR_NOT_CONTRACT);
+    function setDandelionVoting(address _dandelionVoting) external auth(SET_DANDELION_VOTING_ROLE) {
+        require(isContract(_dandelionVoting), ERROR_NOT_CONTRACT);
 
-        dissentVoting = DissentVoting(_dissentVoting);
-        emit DissentVotingSet(_dissentVoting);
+        dandelionVoting = DandelionVoting(_dandelionVoting);
+        emit DandelionVotingSet(_dandelionVoting);
     }
 
     /**
-    * @notice Update the dissent window to `_dissentWindow`
-    * @param _dissentWindow New dissent window
+    * @notice Update the dissent window to `_dissentWindowBlocks`
+    * @param _dissentWindowBlocks New dissent window
     */
-    function setDissentWindow(uint256 _dissentWindow) external auth(SET_DISSENT_WINDOW_ROLE) {
-        dissentWindow = _dissentWindow;
-        emit DissentWindowSet(_dissentWindow);
+    function setDissentWindow(uint64 _dissentWindowBlocks) external auth(SET_DISSENT_WINDOW_ROLE) {
+        dissentWindowBlocks = _dissentWindowBlocks;
+        emit DissentWindowBlocksSet(_dissentWindowBlocks);
     }
 
+    /**
+    * @notice ACLOracle
+    * @dev IACLOracle interface conformance
+    */
     function canPerform(address who, address where, bytes32 what, uint256[] how) external view returns (bool) {
-        return dissentVoting.lastYeaVoteTime(who) + dissentWindow < getTimestamp();
+        // We check hasNeverVotedYea for the edge case where the chains current block number is less than the
+        // dissentWindowBlocks and canPerform would return false even if "who" has not voted, when it should return true.
+        bool hasNeverVotedYea = dandelionVoting.lastYeaVoteBlock(who) == 0;
+
+        // TODO: Consider args passed in 'how'
+
+        return dandelionVoting.lastYeaVoteBlock(who).add(dissentWindowBlocks) < getBlockNumber64() || hasNeverVotedYea;
     }
 
 }
--- dissent-oracle/test/dissentOracle.js
+++ dissent-oracle/test/dissentOracle.js
@@ -17,23 +17,24 @@ const bigExp = (x, y) => new BN(x).mul(new BN(10).pow(new BN(y)))
 const pct16 = x => bigExp(x, 16)
 const createdVoteId = receipt => getLog(receipt, 'StartVote', 'voteId')
 
-// TODO: Add further integration tests with permissions with params
+// TODO: Consider adding further integration tests with permissions with params
 contract('DissentOracle', ([appManager, voter]) => {
 
     let dissentOracle, dissentOracleBase, dandelionVoting, dandelionVotingBase, voteToken
-    let SET_DISSENT_VOTING_ROLE, SET_DISSENT_WINDOW_ROLE, CREATE_VOTES_ROLE
+    let SET_DANDELION_VOTING_ROLE, SET_DISSENT_WINDOW_ROLE, CREATE_VOTES_ROLE
     let dao, acl
 
-    const DISSENT_WINDOW = 60 * 60 * 24 * 2 // 2 days
+    const DISSENT_WINDOW = 60 * 60 * 24 / 15 // 1 day in blocks
     const VOTE_TOKEN_DECIMALS = 18
 
     const neededSupport = pct16(50)
     const minimumAcceptanceQuorum = pct16(20)
-    const votingDuration = 60 * 60 * 24 // 1 day
+    const voteDurationBlocks = 60 * 60 * 24 / 15 // 1 day in blocks
+    const voteBufferBlocks = 100
 
     before('deploy base apps', async () => {
         dissentOracleBase = await DissentOracle.new()
-        SET_DISSENT_VOTING_ROLE = await dissentOracleBase.SET_DISSENT_VOTING_ROLE()
+        SET_DANDELION_VOTING_ROLE = await dissentOracleBase.SET_DANDELION_VOTING_ROLE()
         SET_DISSENT_WINDOW_ROLE = await dissentOracleBase.SET_DISSENT_WINDOW_ROLE()
 
         dandelionVotingBase = await DandelionVoting.new()
@@ -56,21 +57,21 @@ contract('DissentOracle', ([appManager, voter]) => {
         dissentOracle = await DissentOracle.at(deployedContract(newDissentOracleReceipt))
 
         await acl.createPermission(appManager, dissentOracle.address, SET_DISSENT_WINDOW_ROLE, appManager, {from: appManager})
-        await acl.createPermission(appManager, dissentOracle.address, SET_DISSENT_VOTING_ROLE, appManager, {from: appManager})
+        await acl.createPermission(appManager, dissentOracle.address, SET_DANDELION_VOTING_ROLE, appManager, {from: appManager})
         await acl.createPermission(ANY_ADDRESS, dandelionVoting.address, CREATE_VOTES_ROLE, appManager, {from: appManager})
 
-        await dandelionVoting.initialize(voteToken.address, neededSupport, minimumAcceptanceQuorum, votingDuration)
+        await dandelionVoting.initialize(voteToken.address, neededSupport, minimumAcceptanceQuorum, voteDurationBlocks, voteBufferBlocks)
     })
 
-    describe("initialize(address _dandelionVoting, uint256 _dissentWindow)", async () => {
+    describe("initialize(address _dandelionVoting, uint64 _dissentWindowBlocks)", async () => {
 
         beforeEach(async () => {
             await dissentOracle.initialize(dandelionVoting.address, DISSENT_WINDOW)
         })
 
         it('should initialize with correct parameters', async () => {
-            const actualDandelionVoting = await dissentOracle.dissentVoting()
-            const actualDissentWindow = await dissentOracle.dissentWindow()
+            const actualDandelionVoting = await dissentOracle.dandelionVoting()
+            const actualDissentWindow = await dissentOracle.dissentWindowBlocks()
 
             assert.strictEqual(actualDandelionVoting, dandelionVoting.address)
             assert.equal(actualDissentWindow, DISSENT_WINDOW)
@@ -84,21 +85,21 @@ contract('DissentOracle', ([appManager, voter]) => {
                     hash('dandelion-voting.aragonpm.test'), dandelionVotingBase.address, '0x', false, {from: appManager})
                 const newDandelionVoting = await DandelionVoting.at(deployedContract(newDandelionVotingReceipt))
 
-                await dissentOracle.setDissentVoting(newDandelionVoting.address)
+                await dissentOracle.setDandelionVoting(newDandelionVoting.address)
 
-                const actualDissentOracle = await dissentOracle.dissentVoting()
+                const actualDissentOracle = await dissentOracle.dandelionVoting()
                 assert.strictEqual(actualDissentOracle, newDandelionVoting.address)
             })
         })
 
-        describe('setDissentWindow(uint256 _dissentWindow)', () => {
+        describe('setDissentWindow(uint64 _dissentWindowBlocks)', () => {
 
             it('sets a new dissent window', async () => {
                 const expectedDissentWindow = 100
 
                 await dissentOracle.setDissentWindow(expectedDissentWindow)
 
-                const actualDissentWindow = await dissentOracle.dissentWindow()
+                const actualDissentWindow = await dissentOracle.dissentWindowBlocks()
                 assert.equal(actualDissentWindow, expectedDissentWindow)
             })
         })
@@ -109,33 +110,33 @@ contract('DissentOracle', ([appManager, voter]) => {
 
             beforeEach(async () => {
                 await voteToken.generateTokens(voter, bigExp(20, VOTE_TOKEN_DECIMALS))
-                voteId = createdVoteId(await dandelionVoting.newVote(encodeCallScript([]), ''), {from: appManager})
+                voteId = createdVoteId(await dandelionVoting.newVote(encodeCallScript([]), '', false), {from: appManager})
             })
 
-            it('returns true when last yea vote after dissent window', async () => {
-                await dandelionVoting.vote(voteId, true, false, {from: voter})
-                await dissentOracle.mockIncreaseTime(DISSENT_WINDOW + 1)
+            it('returns true when voted yea and dissent window passed', async () => {
+                await dandelionVoting.vote(voteId, true, {from: voter})
+                await dissentOracle.mockAdvanceBlocks(DISSENT_WINDOW)
                 const actualCanPerform = await dissentOracle.canPerform(voter, ANY_ADDRESS, '0x', [])
                 assert.isTrue(actualCanPerform)
             })
 
-            it('returns true when last vote no but within dissent window', async () => {
-                await dandelionVoting.vote(voteId, false, false, {from: voter})
+            it('returns false when voted yea and before end of dissent window', async () => {
+                await dandelionVoting.vote(voteId, true, {from: voter})
+                await dissentOracle.mockAdvanceBlocks(DISSENT_WINDOW - 2)
+                const actualCanPerform = await dissentOracle.canPerform(voter, ANY_ADDRESS, '0x', [])
+                assert.isFalse(actualCanPerform)
+            })
+
+            it('returns false when voted yea within dissent window', async () => {
+                await dandelionVoting.vote(voteId, true, {from: voter})
+                const actualCanPerform = await dissentOracle.canPerform(voter, ANY_ADDRESS, '0x', [])
+                assert.isFalse(actualCanPerform)
+            })
+
+            it('returns true when voted no but within dissent window', async () => {
+                await dandelionVoting.vote(voteId, false, {from: voter})
                 const actualCanPerform = await dissentOracle.canPerform(voter, ANY_ADDRESS, '0x', [])
                 assert.isTrue(actualCanPerform)
-            })
-
-            it('returns false when last yea vote within dissent window', async () => {
-                await dandelionVoting.vote(voteId, true, false, {from: voter})
-                const actualCanPerform = await dissentOracle.canPerform(voter, ANY_ADDRESS, '0x', [])
-                assert.isFalse(actualCanPerform)
-            })
-
-            it('returns false when last yea vote just before end of dissent window', async () => {
-                await dandelionVoting.vote(voteId, true, false, {from: voter})
-                await dissentOracle.mockIncreaseTime(DISSENT_WINDOW - 1)
-                const actualCanPerform = await dissentOracle.canPerform(voter, ANY_ADDRESS, '0x', [])
-                assert.isFalse(actualCanPerform)
             })
         })
     })
