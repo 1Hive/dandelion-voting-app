b0645064a01c98daace52f4a2afbc26e36031d21
William Griffiths <willjgriff@users.noreply.github.com>
2019-10-30 14:52:25 +0000

    Update dandelion-voting/test/voting.js
    Co-Authored-By: Fabrizio Vigevani <fabriziovigevani@gmail.com>

--- dandelion-voting/test/voting.js
+++ dandelion-voting/test/voting.js
@@ -313,7 +313,7 @@ contract('Voting App', ([root, holder1, holder2, holder20, holder29, holder51, n
                     const state = await voting.getVote(voteId)
 
                     const currentBalance = await token.balanceOf(holder29)
-                    assert.equal(state[7].toString(), bigExp(28, decimals).toString(), 'snapshot balance should have been added')
+                    assert.equal(state[7].toString(), bigExp(28, decimals).toString(), 'current balance should have been added')
                     assert.equal(currentBalance.toNumber(), bigExp(28, decimals).toNumber(), 'balance should be 28 at current block')
                 })
 
