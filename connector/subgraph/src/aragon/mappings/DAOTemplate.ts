a71c01a1a0c71353b4ce6def24a31cfa15a448a9
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-07-02 13:44:47 -0300

    change dao factory name

--- connector/subgraph/src/aragon/mappings/DAOTemplate.ts
+++ connector/subgraph/src/aragon/mappings/DAOTemplate.ts
@@ -1,19 +1,21 @@
-import { DeployDao as DeployDaoEvent } from '../../../generated/bare-template.aragonpm.eth@1.0.0/DAOTemplate'
-import { SetupDao as SetupDaoEvent } from '../../../generated/bare-template.aragonpm.eth@1.0.0/DAOTemplate'
-import { DeployToken as DeployTokenEvent } from '../../../generated/bare-template.aragonpm.eth@1.0.0/DAOTemplate'
-import { InstalledApp as InstalledAppEvent } from '../../../generated/bare-template.aragonpm.eth@1.0.0/DAOTemplate'
-import * as aragon from '../aragon'
+//TODO - Commented for now need to check with gaby what is this file for
 
-export function handleDeployDao(event: DeployDaoEvent): void {
-  aragon.processOrg(event.params.dao)
-}
+// import { DeployDao as DeployDaoEvent } from '../../../generated/bare-template.aragonpm.eth@1.0.0/DAOTemplate'
+// import { SetupDao as SetupDaoEvent } from '../../../generated/bare-template.aragonpm.eth@1.0.0/DAOTemplate'
+// import { DeployToken as DeployTokenEvent } from '../../../generated/bare-template.aragonpm.eth@1.0.0/DAOTemplate'
+// import { InstalledApp as InstalledAppEvent } from '../../../generated/bare-template.aragonpm.eth@1.0.0/DAOTemplate'
+// import * as aragon from '../aragon'
 
-export function handleInstalledApp(event: InstalledAppEvent): void {
-  aragon.processApp(event.params.appProxy, event.params.appId.toHexString())
-}
+// export function handleDeployDao(event: DeployDaoEvent): void {
+//   aragon.processOrg(event.params.dao)
+// }
 
-export function handleDeployToken(event: DeployTokenEvent): void {
-  aragon.processToken(event.params.token)
-}
+// export function handleInstalledApp(event: InstalledAppEvent): void {
+//   aragon.processApp(event.params.appProxy, event.params.appId.toHexString())
+// }
 
-export function handleSetupDao(event: SetupDaoEvent): void {}
+// export function handleDeployToken(event: DeployTokenEvent): void {
+//   aragon.processToken(event.params.token)
+// }
+
+// export function handleSetupDao(event: SetupDaoEvent): void {}
