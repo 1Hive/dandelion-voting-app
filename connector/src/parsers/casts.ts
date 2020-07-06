351c5feda1b858ad3ee093f37044f5cefaac79aa
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-07-06 20:48:49 -0300

    cast parser

---
+++ connector/src/parsers/casts.ts
@@ -0,0 +1,29 @@
+import { QueryResult } from '@aragon/connect-thegraph'
+import Cast, { CastData } from '../entities/Cast'
+
+export function parseCasts(
+  result: QueryResult,
+  connector: any
+): Cast[] {
+  const casts = result.data.casts
+
+  if (!casts) {
+    throw new Error('Unable to parse casts.')
+  }
+
+  const datas = casts.map(
+    (cast: any): CastData => {
+      return {
+        id: cast.id,
+        voteId: cast.voteId,
+        voter: cast.voter,
+        supports: cast.supports,
+        voterStake: cast.voterStake
+      }
+    }
+  )
+
+  return datas.map((data: CastData) => {
+    return new Cast(data, connector)
+  })
+}
