3eeb6ab25e907f317cb2576704a21f38c0a8cb38
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-07-06 19:19:33 -0300

    Cast entity

---
+++ connector/src/entities/Cast.ts
@@ -0,0 +1,24 @@
+import DandelionVotingEntity from "./DandelionVotingEntity";
+import VotingConnectorTheGraph from "../connector";
+
+export interface CastData {
+  id: string
+  voteId: string
+  voter: string
+  supports: boolean
+  voterStake: string
+}
+
+export default class Cast extends DandelionVotingEntity implements CastData {
+  readonly id!: string
+  readonly voteId!: string
+  readonly voter!: string
+  readonly supports!: boolean
+  readonly voterStake!: string
+
+  constructor(data: CastData, connector: VotingConnectorTheGraph) {
+    super(connector)
+
+    Object.assign(this, data)
+  }
+}
